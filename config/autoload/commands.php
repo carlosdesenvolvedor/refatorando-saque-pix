e5dd3baa4c8787ac9c5329e8b74be84c0c7e4a75
carlos cezar nunes de souza <carlosjsciaa@gmail.com>
2025-11-28 15:09:33 -0300

    fix: register all migration commands manually

--- config/autoload/commands.php
+++ config/autoload/commands.php
@@ -11,4 +11,10 @@ declare(strict_types=1);
  */
 return [
     Hyperf\Database\Commands\Migrations\MigrateCommand::class,
+    Hyperf\Database\Commands\Migrations\InstallCommand::class,
+    Hyperf\Database\Commands\Migrations\RollbackCommand::class,
+    Hyperf\Database\Commands\Migrations\ResetCommand::class,
+    Hyperf\Database\Commands\Migrations\RefreshCommand::class,
+    Hyperf\Database\Commands\Migrations\FreshCommand::class,
+    Hyperf\Database\Commands\Migrations\StatusCommand::class,
 ];
